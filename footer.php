60a377c563977b8a29ee37494187f630c5711cde
Nathan Campos <eeepc904@gmail.com>
2020-08-30 16:17:12 +0000

    Changed the templates to mimic Levissimo as much as possible

--- footer.php
+++ footer.php
@@ -1,10 +1,3 @@
+<div id="copyright">
+	&copy; <?php echo esc_html( date_i18n( __( 'Y', 'levissimo' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
-<footer id="footer">
-	<div id="copyright">
-		&copy; <?php echo esc_html( date_i18n( __( 'Y', 'levissimo' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-	</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
--- index.php
+++ index.php
@@ -1,10 +1,38 @@
-<?php get_header(); ?>
-<main id="content">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part( 'entry' ); ?>
-		<?php comments_template(); ?>
-	<?php endwhile; endif; ?>
-	<?php get_template_part( 'nav', 'below' ); ?>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<!--[if IE]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	<![endif]-->
+
+	<!-- Header -->
+	<header id="header">
+		<?php get_header(); ?>
+	</header>
+
+	<!-- Articles -->
+	<main class="article-container">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'entry' ); ?>
+			<?php comments_template(); ?>
+		<?php endwhile; endif; ?>
+		<?php get_template_part( 'nav', 'below' ); ?>
+	</main>
+
+	<!-- Sidebar -->
+	<!--<aside id="sidebar">
+		<?php get_sidebar(); ?>
+	</aside>-->
+
+	<!-- Footer -->
+	<footer id="footer">
+		<?php get_footer(); ?>
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>
