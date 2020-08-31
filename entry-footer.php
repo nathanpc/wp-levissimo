6f2dd6ee844f547e406abe681d540e7cb11a03b5
Nathan Campos <eeepc904@gmail.com>
2020-08-31 15:53:54 +0000

    Improved the entry footer comments label

--- entry-footer.php
+++ entry-footer.php
@@ -6,7 +6,7 @@
 <span class="tag-links"><?php the_tags(); ?></span>
 
 <?php
-	if (comments_open()) {
-		echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . esc_url(get_comments_link()) . '">' . sprintf(esc_html__('Comments', 'levissimo')) . '</a></span>';
+	if (comments_open() && (get_comments_number() > 0)) {
+		echo ' - <span class="comments-link"><a href="' . esc_url(get_comments_link()) . '">' . sprintf(esc_html__('%d Comments', 'levissimo'), get_comments_number()) . '</a></span>';
 	}
 ?>
