ba9f7a527220755c4989e27e7e989976eedc1ca9
Nathan Campos <eeepc904@gmail.com>
2020-08-31 19:55:57 +0000

    Fixed the comment section styling

--- single.php
+++ single.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php get_template_part('entry'); ?>
-		<?php if (comments_open() && !post_password_required()) { comments_template('', true); } ?>
-		<?php endwhile; endif; ?>
+	<?php
+		if (have_posts()) : while (have_posts()) : the_post();
+			get_template_part('entry');
+
+			if (comments_open() && !post_password_required()) {
+				comments_template(); 
+			}
+		endwhile; endif;
+	?>
 
 <?php get_footer(); ?>
