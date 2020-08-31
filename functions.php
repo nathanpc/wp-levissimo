0e20321ff569ffdbd027b5e3ce66725fbf16b165
Nathan Campos <eeepc904@gmail.com>
2020-08-31 12:49:41 +0000

    Fixed the formatting of functions.php

--- functions.php
+++ functions.php
@@ -1,116 +1,128 @@
 <?php
-add_action( 'after_setup_theme', 'levissimo_setup' );
+add_action('after_setup_theme', 'levissimo_setup');
 function levissimo_setup() {
-load_theme_textdomain( 'levissimo', get_template_directory() . '/languages' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'html5', array( 'search-form' ) );
-global $content_width;
-if ( ! isset( $content_width ) ) { $content_width = 1920; }
-register_nav_menus( array( 'main-menu' => esc_html__( 'Main Menu', 'levissimo' ) ) );
+	load_theme_textdomain('levissimo', get_template_directory() . '/languages');
+	add_theme_support('title-tag');
+	add_theme_support('automatic-feed-links');
+	add_theme_support('post-thumbnails');
+	add_theme_support('html5', array('search-form'));
+	global $content_width;
+	if (! isset($content_width)) { $content_width = 1920; }
+	register_nav_menus(array('main-menu' => esc_html__('Main Menu', 'levissimo')));
 }
-add_action( 'wp_enqueue_scripts', 'levissimo_load_scripts' );
+
+add_action('wp_enqueue_scripts', 'levissimo_load_scripts');
 function levissimo_load_scripts() {
-wp_enqueue_style( 'levissimo-style', get_stylesheet_uri() );
-wp_enqueue_script( 'jquery' );
+	wp_enqueue_style('levissimo-style', get_stylesheet_uri());
+	wp_enqueue_script('jquery');
 }
-add_action( 'wp_footer', 'levissimo_footer_scripts' );
+
+add_action('wp_footer', 'levissimo_footer_scripts');
 function levissimo_footer_scripts() {
-?>
-<script>
-jQuery(document).ready(function ($) {
-var deviceAgent = navigator.userAgent.toLowerCase();
-if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
-$("html").addClass("ios");
-$("html").addClass("mobile");
+	?>
+	<script>
+		jQuery(document).ready(function ($) {
+			var deviceAgent = navigator.userAgent.toLowerCase();
+			if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
+				$("html").addClass("ios");
+				$("html").addClass("mobile");
+			}
+			if (navigator.userAgent.search("MSIE") >= 0) {
+				$("html").addClass("ie");
+			}
+			else if (navigator.userAgent.search("Chrome") >= 0) {
+				$("html").addClass("chrome");
+			}
+			else if (navigator.userAgent.search("Firefox") >= 0) {
+				$("html").addClass("firefox");
+			}
+			else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+				$("html").addClass("safari");
+			}
+			else if (navigator.userAgent.search("Opera") >= 0) {
+				$("html").addClass("opera");
+			}
+		});
+	</script>
+	<?php
 }
-if (navigator.userAgent.search("MSIE") >= 0) {
-$("html").addClass("ie");
+
+add_filter('document_title_separator', 'levissimo_document_title_separator');
+function levissimo_document_title_separator($sep) {
+	$sep = '|';
+	return $sep;
 }
-else if (navigator.userAgent.search("Chrome") >= 0) {
-$("html").addClass("chrome");
+
+add_filter('the_title', 'levissimo_title');
+function levissimo_title($title) {
+	if ($title == '') {
+		return '...';
+	} else {
+		return $title;
+	}
 }
-else if (navigator.userAgent.search("Firefox") >= 0) {
-$("html").addClass("firefox");
-}
-else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-$("html").addClass("safari");
-}
-else if (navigator.userAgent.search("Opera") >= 0) {
-$("html").addClass("opera");
-}
-});
-</script>
-<?php
-}
-add_filter( 'document_title_separator', 'levissimo_document_title_separator' );
-function levissimo_document_title_separator( $sep ) {
-$sep = '|';
-return $sep;
-}
-add_filter( 'the_title', 'levissimo_title' );
-function levissimo_title( $title ) {
-if ( $title == '' ) {
-return '...';
-} else {
-return $title;
-}
-}
-add_filter( 'the_content_more_link', 'levissimo_read_more_link' );
+
+add_filter('the_content_more_link', 'levissimo_read_more_link');
 function levissimo_read_more_link() {
-if ( ! is_admin() ) {
-return ' <a href="' . esc_url( get_permalink() ) . '" class="more-link">...</a>';
+	if (! is_admin()) {
+		return ' <a href="' . esc_url(get_permalink()) . '" class="more-link">...</a>';
+	}
 }
+
+add_filter('excerpt_more', 'levissimo_excerpt_read_more_link');
+function levissimo_excerpt_read_more_link($more) {
+	if (! is_admin()) {
+		global $post;
+		return ' <a href="' . esc_url(get_permalink($post->ID)) . '" class="more-link">...</a>';
+	}
 }
-add_filter( 'excerpt_more', 'levissimo_excerpt_read_more_link' );
-function levissimo_excerpt_read_more_link( $more ) {
-if ( ! is_admin() ) {
-global $post;
-return ' <a href="' . esc_url( get_permalink( $post->ID ) ) . '" class="more-link">...</a>';
+
+add_filter('intermediate_image_sizes_advanced', 'levissimo_image_insert_override');
+function levissimo_image_insert_override($sizes) {
+	unset($sizes['medium_large']);
+	return $sizes;
 }
-}
-add_filter( 'intermediate_image_sizes_advanced', 'levissimo_image_insert_override' );
-function levissimo_image_insert_override( $sizes ) {
-unset( $sizes['medium_large'] );
-return $sizes;
-}
-add_action( 'widgets_init', 'levissimo_widgets_init' );
+
+add_action('widgets_init', 'levissimo_widgets_init');
 function levissimo_widgets_init() {
-register_sidebar( array(
-'name' => esc_html__( 'Sidebar Widget Area', 'levissimo' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => '</li>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
+	register_sidebar(array(
+		'name' => esc_html__('Sidebar Widget Area', 'levissimo'),
+		'id' => 'primary-widget-area',
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	));
 }
-add_action( 'wp_head', 'levissimo_pingback_header' );
+
+add_action('wp_head', 'levissimo_pingback_header');
 function levissimo_pingback_header() {
-if ( is_singular() && pings_open() ) {
-printf( '<link rel="pingback" href="%s" />' . "\n", esc_url( get_bloginfo( 'pingback_url' ) ) );
+	if (is_singular() && pings_open()) {
+		printf('<link rel="pingback" href="%s" />' . "\n", esc_url(get_bloginfo('pingback_url')));
+	}
 }
-}
-add_action( 'comment_form_before', 'levissimo_enqueue_comment_reply_script' );
+
+add_action('comment_form_before', 'levissimo_enqueue_comment_reply_script');
 function levissimo_enqueue_comment_reply_script() {
-if ( get_option( 'thread_comments' ) ) {
-wp_enqueue_script( 'comment-reply' );
+	if (get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
+	}
 }
+
+function levissimo_custom_pings($comment) {
+	?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
+	<?php
 }
-function levissimo_custom_pings( $comment ) {
-?>
-<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
-<?php
-}
-add_filter( 'get_comments_number', 'levissimo_comment_count', 0 );
-function levissimo_comment_count( $count ) {
-if ( ! is_admin() ) {
-global $id;
-$get_comments = get_comments( 'status=approve&post_id=' . $id );
-$comments_by_type = separate_comments( $get_comments );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
-}
+
+add_filter('get_comments_number', 'levissimo_comment_count', 0);
+function levissimo_comment_count($count) {
+	if (! is_admin()) {
+		global $id;
+		$get_comments = get_comments('status=approve&post_id=' . $id);
+		$comments_by_type = separate_comments($get_comments);
+		return count($comments_by_type['comment']);
+	} else {
+		return $count;
+	}
 }
