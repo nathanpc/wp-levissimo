938c7527cac6b88853a190a95d704fdc0692094a
Nathan Campos <eeepc904@gmail.com>
2020-09-01 13:57:40 +0000

    Fixed the layout of the 404 page

--- 404.php
+++ 404.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
-<main id="content">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php esc_html_e( 'Not Found', 'levissimo' ); ?></h1>
-</header>
-<div class="entry-content">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'levissimo' ); ?></p>
-<?php get_search_form(); ?>
-</div>
-</article>
-</main>
-<?php get_sidebar(); ?>
+
+			<article id="post-0" class="post not-found">
+				<h2><?php esc_html_e('Not Found', 'levissimo'); ?></h2>
+
+				<div class="article-body">
+					<p><?php esc_html_e('Nothing found for the requested page. Try a search instead?', 'levissimo'); ?></p>
+				</div>
+			</article>
+
+			<hr>
+
 <?php get_footer(); ?>
